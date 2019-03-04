0b063c87d3648a1177022429ba3d9cd0498fd13f
Ariel <arielord@buffalo.edu>
2019-03-04 22:28:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,18 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  attr_accessor :name, :grade, 
+  attr_accessor :name, :grade, :id
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+  end
+  
+  def self.create_table
+    sql = 
+  end
+  
+  def save
+    
+  end
 end
