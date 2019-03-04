09efc22d448d786c0526be23ca5c530bb33a394e
Ariel <arielord@buffalo.edu>
2019-03-04 22:30:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,9 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students
+      CREATE TABLE IF NOT EXISTS students (
       
+      )
     SQL
   end
   
